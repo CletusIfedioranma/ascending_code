fcd29e2c09e5f6d4b088ac5d864dc5402d008c1f
cletusIfedioranma <cletus.ifedioranma@gmail.com>
2023-09-15 10:57:22 +0100

    second commit

---
+++ Ascending code/sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    const len = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    console.log(arr);
+}
+bubbleSort([5,2,0,-3,4,5,2,10,40,3,4])
